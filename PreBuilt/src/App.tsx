ed49fc6f4fcb623220a7d80096ef4e9db98e2c8c
Nathan Lee <nsjlee33@hotmail.com>
2024-01-21 00:39:54 -0800

    scuffed frontend changes

--- PreBuilt/src/App.tsx
+++ PreBuilt/src/App.tsx
@@ -1,25 +1,41 @@
+import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Camera from "./Camera";
 import Navigation from "./Navigation";
 import Basic from "./Basic";
 import Interactivity from "./Interactivity";
+import ClientButton from "./ClientButton";
+import "./styles.css"; // Import the CSS file for styling
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div style={{ display: "flex", gap: "12px" }}>
-        Examples:
-        <Link to="/">Basic</Link>
-        <Link to="/interactivity">Interactivity</Link>
-        <Link to="/navigation">Navigation</Link>
-        <Link to="/camera">Camera</Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<Basic />} />
-        <Route path="/interactivity" element={<Interactivity />} />
-        <Route path="/navigation" element={<Navigation />} />
-        <Route path="/camera" element={<Camera />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <div className="ui-container">
+          {/* Links centered on top */}
+          <div className="top-links">
+            <Link to="/">Basic</Link>
+            <Link to="/interactivity">Interactivity</Link>
+            <Link to="/navigation">Navigation</Link>
+            <Link to="/camera">Camera</Link>
+          </div>
+          
+          {/* Routes centered in the middle */}
+          <div className="middle-routes">
+            <Routes>
+              <Route path="/" element={<Basic />} />
+              <Route path="/interactivity" element={<Interactivity />} />
+              <Route path="/navigation" element={<Navigation />} />
+              <Route path="/camera" element={<Camera />} />
+            </Routes>
+          </div>
+          
+          {/* Button centered at the bottom */}
+          <div className="bottom-button">
+            <ClientButton />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
---
+++ PreBuilt/src/ClientButton.tsx
@@ -0,0 +1,11 @@
+import "./ClientButton.css";
+
+export default function SlideButton() {
+  return (
+    <div>
+        <img src="src/images/img1version1.jpg"/>
+        <img src="src/images/img2version1.jpg"/>
+        <img src="src/images/img3version1.jpg"/>
+    </div>
+  );
+}
